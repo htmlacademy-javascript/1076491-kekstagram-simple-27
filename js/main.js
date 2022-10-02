664eaa34e292c9546a323e5e3963751cc5ea7515
Evgen <saintapostle@yandex.ru>
2022-10-02 09:21:19 +0300

    Убрал элементы которые могут запутать

--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно.
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-
   if (min < 0 || max < 0) {
     return NaN;
   }
@@ -17,9 +14,6 @@ function maxlengthStr(str, minlength, maxlength) {
 
   if (str.length < minlength || str.length > maxlength) {
     return false;
-    // alert(
-    //   "Длина комментария должна быть не меньше 20 символов и не больше 140 символов"
-    // );
   } else {
     return str;
   }
