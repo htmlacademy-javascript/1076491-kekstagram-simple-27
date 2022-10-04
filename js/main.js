dee928be7a2f11cb42986094ee6215a076c3da71
Evgen <saintapostle@yandex.ru>
2022-10-04 12:16:31 +0300

    Замена двойных кавычек на одинарные

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
 const idPhoto = [];
 const urlPhoto = [
-  "photos/1.jpg",
-  "photos/2.jpg",
-  "photos/3.jpg",
-  "photos/4.jpg",
-  "photos/5.jpg",
-  "photos/6.jpg",
-  "photos/7.jpg",
-  "photos/8.jpg",
-  "photos/9.jpg",
-  "photos/10.jpg",
-  "photos/11.jpg",
-  "photos/12.jpg",
-  "photos/13.jpg",
-  "photos/14.jpg",
-  "photos/15.jpg",
-  "photos/16.jpg",
-  "photos/17.jpg",
-  "photos/18.jpg",
-  "photos/19.jpg",
-  "photos/20.jpg",
-  "photos/21.jpg",
-  "photos/22.jpg",
-  "photos/23.jpg",
-  "photos/24.jpg",
-  "photos/25.jpg",
+  'photos/1.jpg',
+  'photos/2.jpg',
+  'photos/3.jpg',
+  'photos/4.jpg',
+  'photos/5.jpg',
+  'photos/6.jpg',
+  'photos/7.jpg',
+  'photos/8.jpg',
+  'photos/9.jpg',
+  'photos/10.jpg',
+  'photos/11.jpg',
+  'photos/12.jpg',
+  'photos/13.jpg',
+  'photos/14.jpg',
+  'photos/15.jpg',
+  'photos/16.jpg',
+  'photos/17.jpg',
+  'photos/18.jpg',
+  'photos/19.jpg',
+  'photos/20.jpg',
+  'photos/21.jpg',
+  'photos/22.jpg',
+  'photos/23.jpg',
+  'photos/24.jpg',
+  'photos/25.jpg',
 ];
 const description = [
-  "получилась фотография лучше",
-  "Всё отлично!",
-  "придумайте название самостоятельно",
-  "В целом всё неплохо",
-  "такой неудачный момент!",
+  'получилась фотография лучше',
+  'Всё отлично!',
+  'придумайте название самостоятельно',
+  'В целом всё неплохо',
+  'такой неудачный момент!',
 ];
 const likes = [];
 const comments = [];
@@ -71,3 +71,5 @@ const createCard = () => ({
 });
 
 const totalCards = Array.from({ length: cardCount }, createCard);
+
+console.log(totalCards);
